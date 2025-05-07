9f7bb553988ff99d2cd6f840fd9bc9b2a09fc0d3
Adithya Balaji <adithyabsk@gmail.com>
2025-05-06 20:18:28 -0700

    bug: fix missing timestamps

--- src/logger.js
+++ src/logger.js
@@ -1,17 +1,16 @@
 const winston = require('winston');
+const { format } = winston;
 
 let logLevel = process.env.DEBUG === 'true' ? 'debug' : 'info';
 
 const logger = winston.createLogger({
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
     transports: [
         new winston.transports.Console({
-            level: logLevel,
-            timestamp: function () {
-                return (new Date()).toISOString();
-            },
-            json: true,
-            // This makes sure each log entry is single line
-            stringify: (obj) => JSON.stringify(obj)
+            level: logLevel
         })
     ]
 });
